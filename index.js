b35fec8e334f09e8ea45d7cd21f9b8146ec1a870
Matthew Bronstein <mbronstein1@gmail.com>
2022-09-25 17:55:19 -0500

    finished ugly version of logic. Need to do massive refactoring to create clean modules, but at least it works as is

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const inquirer = require("inquirer");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
-const employees = [];
+// const renderHtml = require("./src/html_template");
+// const employees = [];
 
 const init = () => {
     inquirer
@@ -18,7 +19,7 @@ const init = () => {
         .then((ans) => {
             console.log(ans)
             if (ans.title === "I do not work here") {
-                console.log("\x1b[33m Sorry for the confusion. Have a good day!");
+                console.log("\x1b[33m Sorry for the confusion. Have a nice day!");
                 return;
             } else if (ans.title === "Manager") {
                 managerInquirer()
@@ -69,7 +70,7 @@ const managerInquirer = () => {
             },
             {
                 type: "input",
-                message: "What is your email?",
+                message: "What is your email address?",
                 name: "email",
                 validate: (input) => {
                     if (input.includes(".", "@")) {
@@ -106,13 +107,13 @@ const engineerInquirer = () => {
         .prompt([
             {
                 type: "input",
-                message: "What is your name?",
+                message: "What is your engineer's name?",
                 name: "name",
-                validate: (input) => input ? true : console.log("\x1b[31m Please enter your name")
+                validate: (input) => input ? true : console.log("\x1b[31m Please enter a name")
             },
             {
                 type: "input",
-                message: "What is your ID?",
+                message: "What is your engineer's ID number?",
                 name: "id",
                 validate: (input) => {
                     if (!isNaN(input)) {
@@ -126,7 +127,7 @@ const engineerInquirer = () => {
             },
             {
                 type: "input",
-                message: "What is your email?",
+                message: "What is your engineer's email address?",
                 name: "email",
                 validate: (input) => {
                     if (input.includes(".", "@")) {
@@ -139,7 +140,7 @@ const engineerInquirer = () => {
             },
             {
                 type: "input",
-                message: "What is your GitHub profile name?",
+                message: "What is your engineer's GitHub profile name?",
                 name: "github",
             },
         ])
@@ -155,13 +156,13 @@ const internInquirer = () => {
         .prompt([
             {
                 type: "input",
-                message: "What is your name?",
+                message: "What is your intern's name?",
                 name: "name",
                 validate: (input) => input ? true : console.log("\x1b[31m Please enter your name")
             },
             {
                 type: "input",
-                message: "What is your ID?",
+                message: "What is your intern's ID number?",
                 name: "id",
                 validate: (input) => {
                     if (!isNaN(input)) {
@@ -175,7 +176,7 @@ const internInquirer = () => {
             },
             {
                 type: "input",
-                message: "What is your email?",
+                message: "What is your intern's email address?",
                 name: "email",
                 validate: (input) => {
                     if (input.includes(".", "@")) {
@@ -188,7 +189,7 @@ const internInquirer = () => {
             },
             {
                 type: "input",
-                message: "What school do you go to?",
+                message: "What school did/does your intern attend?",
                 name: "school",
             },
         ])
@@ -199,23 +200,124 @@ const internInquirer = () => {
         })
 }
 
+// const managerInfo = (ans) => {
+//     let { name, id, email, officeNumber } = ans;
+//     let manager = new Manager(name, +id, email, +officeNumber);
+//     employees.push(manager);
+// }
+
 const managerInfo = (ans) => {
     let { name, id, email, officeNumber } = ans;
     let manager = new Manager(name, +id, email, +officeNumber);
-    employees.push(manager);
+    fs.writeFile("./dist/index.html", `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <title>My Team Profile</title>
+    </head>
+    <body class = "vh-100">
+        <div class="jumbotron jumbotron-fluid bg-danger">
+            <div class="container d-flex justify-content-center">
+              <h1 class="display-3 text-white p-4">My Team</h1>
+            </div>
+          </div>
+          <div class="card-deck m-5 d-flex justify-content-center">
+    <div class="card mw-40 m-3 shadow">
+    <div class="card-header bg-primary text-white">
+      <h2 class="card-title">${name}</h2>
+      <h3>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body bg-light">
+        <div class="card">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">ID : ${id}</li>
+              <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+              <li class="list-group-item">Office number: ${officeNumber}</li>
+            </ul>
+          </div>
+    </div>
+  </div>
+    `, 
+    ((err) => {
+        if(err) {
+            console.error(err)
+        }}))
+    // employees.push(manager);
 }
+
+// const engineerInfo = (ans) => {
+//     let { name, id, email, github } = ans;
+//     let engineer = new Engineer(name, +id, email, `https://www.github.com/${github}`);
+//     employees.push(engineer);
+// }
 
 const engineerInfo = (ans) => {
     let { name, id, email, github } = ans;
-    let engineer = new Engineer(name, +id, email, `https://www.github.com/${github}`);
-    employees.push(engineer);
+    let engineer = new Engineer(name, +id, email, github);
+    fs.appendFile("./dist/index.html", 
+    `
+    <div class="card mw-40 m-3 shadow">
+                <div class="card-header bg-primary text-white">
+                  <h2 class="card-title">${name}</h2>
+                  <h3>${engineer.getRole()}</h3>
+                </div>
+                <div class="card-body bg-light">
+                    <div class="card">
+                        <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID : ${id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                        <li class="list-group-item">GitHub: <a href="https://www.github.com/${github}" target="_blank">${github}</a></li>
+                        </ul>
+                      </div>
+                </div>
+              </div>
+    `,
+    ((err) => {
+        if(err) {
+            console.error(err)
+        }}))
+    // employees.push(engineer);
 }
+
+// const internInfo = (ans) => {
+//     let { name, id, email, school } = ans;
+//     let intern = new Intern(name, +id, email, school);
+//     employees.push(intern)
+// }
 
 const internInfo = (ans) => {
     let { name, id, email, school } = ans;
     let intern = new Intern(name, +id, email, school);
-    employees.push(intern)
+    // employees.push(intern)
+    fs.appendFile("./dist/index.html",
+    `
+    <div class="card mw-40 m-3 shadow">
+    <div class="card-header bg-primary text-white">
+      <h2 class="card-title">${name}</h2>
+      <h3>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body bg-light">
+        <div class="card">
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID : ${id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+            <li class="list-group-item">School: ${school}</li>
+            </ul>
+          </div>
+    </div>
+    </div>
+    `,
+    ((err) => {
+        if(err) {
+            console.error(err)
+        }})
+    )
 }
+
 
 const addEmployeeInquirer = () => {
     inquirer
@@ -230,8 +332,13 @@ const addEmployeeInquirer = () => {
         switch (ans.addEmployee) {
             case "I'm finished":
                 console.log("\x1b[33m Thank you very much");
-                fs.writeFile("index.html", renderEmployees, ((err) => {
-                    err ? console.error(err) : console.log("File successfully written!")
+                fs.appendFile("./dist/index.html",
+                `          </div>
+                </body>
+                </html>
+                `,
+                ((err) => {
+                    err ? console.error(err) : console.log("File is complete!")
                 }));
                 break;
             case "Engineer":
@@ -243,6 +350,5 @@ const addEmployeeInquirer = () => {
         }
     })
 }
-
 
 init()
--- src/html_template.js
+++ src/html_template.js
@@ -1,4 +1,40 @@
 const renderEmployees = () => {
-    return `
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <title>My Team Profile</title>
+    </head>
+    <body class = "vh-100">
+        <div class="jumbotron jumbotron-fluid bg-danger">
+            <div class="container d-flex justify-content-center">
+              <h1 class="display-3 text-white p-4">My Team</h1>
+            </div>
+          </div>
+          <div class="card-deck m-5 d-flex justify-content-center">
+            <div class="card mw-40 m-3 shadow">
+                <div class="card-header bg-primary text-white">
+                  <h2 class="card-title"><name</h2>
+                  <h3>title</h3>
+                </div>
+                <div class="card-body bg-light">
+                    <div class="card">
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item">ID</li>
+                          <li class="list-group-item">Email</li>
+                          <li class="list-group-item">Misc</li>
+                        </ul>
+                      </div>
+                </div>
+              </div>
+          </div>
+    </body>
+    </html>
     `
 }
+
+module.exports = renderEmployees;
