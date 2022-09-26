3e8b1004588f20935900ce261909f6f7b2c02fc3
Matthew Bronstein <mbronstein1@gmail.com>
2022-09-26 10:10:10 -0500

    began refactoring

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ const Manager = require("./lib/Manager.js");
 // const renderHtml = require("./src/html_template");
 // const employees = [];
 
+// WHEN I click on an email address in the HTML
+// THEN my default email program opens and populates the TO field of the email with the address
+// WHEN I click on the GitHub username
+// THEN that GitHub profile opens in a new tab
+// WHEN I start the application
+// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+// WHEN I enter the team manager’s name, employee ID, email address, and office number
+// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+// WHEN I select the engineer option
+// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+// WHEN I select the intern option
+// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+// WHEN I decide to finish building my team
+// THEN I exit the application, and the HTML is generated
+
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for my team members and their information
 const init = () => {
     inquirer
         .prompt([
@@ -19,8 +38,9 @@ const init = () => {
         .then((ans) => {
             console.log(ans)
             if (ans.title === "I do not work here") {
-                console.log("\x1b[33m Sorry for the confusion. Have a nice day!");
-                return;
+                console.log("\x1b[33m Sorry for the confusion. Have a nice day!");//notification in yellow
+                process.exit(); //same as commmand+c, exits process
+                // return;
             } else if (ans.title === "Manager") {
                 managerInquirer()
             } else {
@@ -28,14 +48,14 @@ const init = () => {
                     .prompt([
                         {
                             type: "list",
-                            message: ("\x1b[31m --You need a manager to access this file--"),
+                            message: ("\x1b[31m --You need a manager to access this file--"),//notification in red
                             name: "validateManager",
                             choices: ["I am a manager", "I will get a manager and try again"],
                         }
                     ])
                     .then((ans) => {
                         if (ans.validateManager === "I will get a manager and try again") {
-                            console.log("\x1b[33m See you later")
+                            console.log("\x1b[33m See you later")//notification in yellow
                             return;
                         } else {
                             managerInquirer()
@@ -97,7 +117,6 @@ const managerInquirer = () => {
         ])
         .then((ans) => {
             managerInfo(ans);
-            // console.log(employees)
             addEmployeeInquirer();
         })
 }
@@ -146,7 +165,6 @@ const engineerInquirer = () => {
         ])
         .then((ans) => {
             engineerInfo(ans);
-            // console.log(employees)
             addEmployeeInquirer();
         })
 }
@@ -195,160 +213,186 @@ const internInquirer = () => {
         ])
         .then((ans) => {
             internInfo(ans);
-            // console.log(employees)
             addEmployeeInquirer();
         })
+}
+
+const managerInfo = (ans) => {
+    let { name, id, email, officeNumber } = ans;
+    let manager = new Manager(name, +id, email, +officeNumber);
+    allEmployees.push(manager); //WILL EVENTUALLY CALL readyToRender() FUNCTION BELOW WITH renderCard() METHOD
 }
 
 // const managerInfo = (ans) => {
 //     let { name, id, email, officeNumber } = ans;
 //     let manager = new Manager(name, +id, email, +officeNumber);
+//     fs.writeFile("./dist/index.html", `
+//     <!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//         <meta charset="UTF-8">
+//         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+//         <title>My Team Profile</title>
+//         </head>
+//     <body class = "vh-100">
+//     <div class="jumbotron jumbotron-fluid bg-danger">
+//             <div class="container d-flex justify-content-center">
+//             <h1 class="display-3 text-white p-4">My Team</h1>
+//             </div>
+//           </div>
+//           <div class="card-deck m-5 d-flex justify-content-center"> //MOVED TO MANAGER CLASS IN MANAGER.JS TO BE CALLED BY renderCard() METHOD IN readyToRender() FUNCTION BELOW
+//           <div class="card mw-40 m-3 shadow">
+//     <div class="card-header bg-primary text-white">
+//     <h2 class="card-title">${name}</h2>
+//     <h3>${manager.getRole()}</h3>
+//     </div>
+//     <div class="card-body bg-light">
+//     <div class="card">
+//             <ul class="list-group list-group-flush">
+//             <li class="list-group-item">ID : ${id}</li>
+//             <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+//               <li class="list-group-item">Office number: ${officeNumber}</li>
+//               </ul>
+//               </div>
+//     </div>
+//     </div>
+//     `,
+//         ((err) => {
+//             if (err) {
+//                 console.error(err)
+//             }
+//         }))
 //     employees.push(manager);
-// }
-
-const managerInfo = (ans) => {
-    let { name, id, email, officeNumber } = ans;
-    let manager = new Manager(name, +id, email, +officeNumber);
-    fs.writeFile("./dist/index.html", `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-        <title>My Team Profile</title>
-    </head>
-    <body class = "vh-100">
-        <div class="jumbotron jumbotron-fluid bg-danger">
-            <div class="container d-flex justify-content-center">
-              <h1 class="display-3 text-white p-4">My Team</h1>
-            </div>
-          </div>
-          <div class="card-deck m-5 d-flex justify-content-center">
-    <div class="card mw-40 m-3 shadow">
-    <div class="card-header bg-primary text-white">
-      <h2 class="card-title">${name}</h2>
-      <h3>${manager.getRole()}</h3>
-    </div>
-    <div class="card-body bg-light">
-        <div class="card">
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">ID : ${id}</li>
-              <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
-              <li class="list-group-item">Office number: ${officeNumber}</li>
-            </ul>
-          </div>
-    </div>
-  </div>
-    `, 
-    ((err) => {
-        if(err) {
-            console.error(err)
-        }}))
-    // employees.push(manager);
-}
-
-// const engineerInfo = (ans) => {
-//     let { name, id, email, github } = ans;
-//     let engineer = new Engineer(name, +id, email, `https://www.github.com/${github}`);
-//     employees.push(engineer);
 // }
 
 const engineerInfo = (ans) => {
     let { name, id, email, github } = ans;
-    let engineer = new Engineer(name, +id, email, github);
-    fs.appendFile("./dist/index.html", 
-    `
-    <div class="card mw-40 m-3 shadow">
-                <div class="card-header bg-primary text-white">
-                  <h2 class="card-title">${name}</h2>
-                  <h3>${engineer.getRole()}</h3>
-                </div>
-                <div class="card-body bg-light">
-                    <div class="card">
-                        <ul class="list-group list-group-flush">
-                        <li class="list-group-item">ID : ${id}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
-                        <li class="list-group-item">GitHub: <a href="https://www.github.com/${github}" target="_blank">${github}</a></li>
-                        </ul>
-                      </div>
-                </div>
-              </div>
-    `,
-    ((err) => {
-        if(err) {
-            console.error(err)
-        }}))
-    // employees.push(engineer);
+    let engineer = new Engineer(name, +id, email, `https://www.github.com/${github}`);
+    employees.push(engineer); //WILL EVENTUALLY CALL readyToRender() FUNCTION BELOW WITH renderCard() METHOD
 }
 
-// const internInfo = (ans) => {
-//     let { name, id, email, school } = ans;
-//     let intern = new Intern(name, +id, email, school);
-//     employees.push(intern)
+// const engineerInfo = (ans) => {
+//     let { name, id, email, github } = ans;
+//     let engineer = new Engineer(name, +id, email, github);
+//     fs.appendFile("./dist/index.html",
+//         `
+//     <div class="card mw-40 m-3 shadow">
+//     <div class="card-header bg-primary text-white">
+//     <h2 class="card-title">${name}</h2>
+//                   <h3>${engineer.getRole()}</h3>
+//                   </div>
+//                 <div class="card-body bg-light">
+//                 <div class="card">
+//                 <ul class="list-group list-group-flush">
+//                 <li class="list-group-item">ID : ${id}</li>
+//                         <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+//                         <li class="list-group-item">GitHub: <a href="https://www.github.com/${github}" target="_blank">${github}</a></li>
+//                         </ul>
+//                         </div>
+//                         </div>
+//               </div>
+//               `,
+//         ((err) => {
+//             if (err) {
+//                 console.error(err)
+//             }
+//         }))
+//     employees.push(engineer);
 // }
 
 const internInfo = (ans) => {
     let { name, id, email, school } = ans;
     let intern = new Intern(name, +id, email, school);
-    // employees.push(intern)
-    fs.appendFile("./dist/index.html",
-    `
-    <div class="card mw-40 m-3 shadow">
-    <div class="card-header bg-primary text-white">
-      <h2 class="card-title">${name}</h2>
-      <h3>${intern.getRole()}</h3>
-    </div>
-    <div class="card-body bg-light">
-        <div class="card">
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID : ${id}</li>
-            <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
-            <li class="list-group-item">School: ${school}</li>
-            </ul>
-          </div>
-    </div>
-    </div>
-    `,
-    ((err) => {
-        if(err) {
-            console.error(err)
-        }})
-    )
+    allEmployees.push(intern); //WILL EVENTUALLY CALL readyToRender() FUNCTION BELOW WITH renderCard() METHOD
 }
+
+// const internInfo = (ans) => {
+//     let { name, id, email, school } = ans;
+//     let intern = new Intern(name, +id, email, school);
+//     // employees.push(intern)
+//     fs.appendFile("./dist/index.html",
+//         `
+//     <div class="card mw-40 m-3 shadow">
+//     <div class="card-header bg-primary text-white">
+//     <h2 class="card-title">${name}</h2>
+//       <h3>${intern.getRole()}</h3>
+//       </div>
+//       <div class="card-body bg-light">
+//         <div class="card">
+//         <ul class="list-group list-group-flush">
+//         <li class="list-group-item">ID : ${id}</li>
+//             <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+//             <li class="list-group-item">School: ${school}</li>
+//             </ul>
+//           </div>
+//           </div>
+//           </div>
+//     `,
+//         ((err) => {
+//             if (err) {
+//                 console.error(err)
+//             }
+//         })
+//     )
+// }
 
 
 const addEmployeeInquirer = () => {
     inquirer
-    .prompt([
-        {
-            type: "list",
-            message: "Would you like to add an employee?",
-            name: "addEmployee",
-            choices: ["Engineer", "Intern", "I'm finished"],
-        }
-    ]).then((ans) => {
-        switch (ans.addEmployee) {
-            case "I'm finished":
-                console.log("\x1b[33m Thank you very much");
-                fs.appendFile("./dist/index.html",
-                `          </div>
-                </body>
-                </html>
-                `,
-                ((err) => {
-                    err ? console.error(err) : console.log("File is complete!")
-                }));
-                break;
-            case "Engineer":
-                engineerInquirer()
-                break;
-            case "Intern":
-                internInquirer()
-                break;
-        }
-    })
+        .prompt([
+            {
+                type: "list",
+                message: "Would you like to add an employee?",
+                name: "addEmployee",
+                choices: ["Engineer", "Intern", "I'm finished"],
+            }
+        ]).then((ans) => {
+            switch (ans.addEmployee) {
+                case "I'm finished":
+                    console.log("\x1b[33m Thank you very much");
+                    fs.writeFile("./dist/index.html", readyToRender, ((err) => err ? console.error(err) : console.log("File is complete!")));
+                    break;
+                case "Engineer":
+                    engineerInquirer()
+                    break;
+                case "Intern":
+                    internInquirer()
+                    break;
+            }
+        })
+}
+// once an employee has been fully answered push a new instance of it to this array.
+var allEmployees = []; // [new Manager(1, john, etc), new Intern(etc), new Engineer(), new Engineer(), new intern()]
+
+function readyToRender() {
+    let html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <title>My Team Profile</title>
+</head>
+    <body class = "vh-100">
+        <div class="jumbotron jumbotron-fluid bg-danger">
+            <div class="container d-flex justify-content-center">
+                <h1 class="display-3 text-white p-4">My Team</h1>
+            </div>
+        </div>
+        <div class="card-deck m-5 d-flex justify-content-center">`
+    for (let employee of allEmployees) {
+        html += employee.renderCard()
+    }
+    html += `
+    </div>
+</body>
+</html>`
+    return html;
 }
 
 init()
+// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
